763c2db6084755a3602f41e3d1a85c8427fbb750
swayvii <tug83551@temple.edu>
2019-05-03 00:38:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lab8/BookDetailsFragment.java
@@ -0,0 +1,84 @@
+package com.example.lab8;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BookDetailsFragment extends Fragment {
+
+    public BookDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    TextView tv;
+    ImageView imageView;
+    String bookSelected;
+    String title;
+    String author;
+    String publish;
+    public static final String BOOK_KEY = "myBook";
+    Book pagerBooks;
+
+    public static BookDetailsFragment newInstance(Book bookList) {
+        BookDetailsFragment fragment = new BookDetailsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(BOOK_KEY, bookList);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getArguments() != null){
+            pagerBooks = getArguments().getParcelable(BOOK_KEY);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_book_details, container, false);
+        tv = view.findViewById(R.id.bookTitle);
+        imageView = view.findViewById(R.id.bookImage);
+        if(getArguments() != null) {
+            displayBook(pagerBooks);
+        }
+
+        return view;
+    }
+
+    public void displayBook(Book bookObj){
+        author = bookObj.getAuthor();
+        title = bookObj.getTitle(); publish = bookObj.getPublished();
+        tv.setText(" \"" + title + "\" "); tv.append(" " + author); tv.append(" " + publish);
+        tv.setTextSize(30);
+        tv.setTextColor(Color.BLACK);
+        String imageURL = bookObj.getCoverURL();
+        Picasso.get().load(imageURL).into(imageView);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/lab8/BookListFragment.java
@@ -0,0 +1,113 @@
+package com.example.lab8;
+
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BookListFragment extends Fragment {
+
+    private BookInterface mListener;
+
+    public BookListFragment() {
+        // Required empty public constructor
+    }
+
+    public static BookListFragment newInstance(String param1, String param2) {
+        BookListFragment fragment = new BookListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    ListView listView;
+    Context c;
+    ArrayList<Book> bookList;
+    Book books;
+    BookAdapter adapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_book_list, container, false);
+
+        listView = v.findViewById(R.id.bookList);
+        bookList = new ArrayList<>();
+
+        return v;
+    }
+
+    public void getBooks(final JSONArray bookArray){
+        bookList.clear();
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter(c, android.R.layout.simple_list_item_1, bookList);
+        for(int i = 0; i < bookArray.length(); i++){
+            try {
+                bookList.add(new Book(bookArray.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.d("Book List", bookList.toString());
+        updateList();
+    }
+
+    private void updateList(){
+        adapter = new BookAdapter(c, bookList);
+        adapter.notifyDataSetChanged();
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                books = bookList.get(position);
+                ((BookInterface) c).bookSelected(books);
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof BookInterface) {
+            mListener = (BookInterface) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+        this.c = context;
+    }
+
+    public interface BookInterface {
+        void bookSelected(Book bookObj);
+    }
+}
